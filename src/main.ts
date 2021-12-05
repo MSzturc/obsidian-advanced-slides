5ba008bd4d35a322fcec7640cfb7a295aaf571d4
MSzturc <MSzturc@googlemail.com>
2021-12-06 00:30:48 +0100

    basic obsidian module that is able to hotreload

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+export default class AdvancedSlidesPlugin extends Plugin {
+
+	async onload() {
+
+	}
+
+	onunload() {
+
+	}
+}
