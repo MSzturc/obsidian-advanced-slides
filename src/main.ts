8abdf1273a6e97b5ac632b0b879286cd3a9c71a6
MSzturc <MSzturc@googlemail.com>
2021-12-06 11:53:13 +0100

    added view to render webpages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,74 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, ItemView, WorkspaceLeaf } from 'obsidian';
 
 export default class AdvancedSlidesPlugin extends Plugin {
 
 	async onload() {
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		);
 
+		this.addRibbonIcon("dice", "Activate view", () => {
+			this.activateView();
+		});
 	}
 
 	onunload() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+	}
 
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+		await this.app.workspace.getLeaf(true).setViewState({
+			type: VIEW_TYPE_EXAMPLE,
+			active: true,
+		});
+
+		this.app.workspace.revealLeaf(
+			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+		);
+	}
+}
+
+export const VIEW_TYPE_EXAMPLE = "example-view";
+
+export class ExampleView extends ItemView {
+
+	iframe: HTMLIFrameElement;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Reveal.js Preview";
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.setAttr('style','padding: 0;');
+
+		const inner = container.createDiv({
+            cls: "markdown-preview-view",
+            attr: { style: "padding: 0;overflow:hidden;" }
+        });
+
+		inner.createEl("iframe",
+			{
+				attr: {
+					src: 'http://www.google.de',
+					style: 'border-style:none;width: 100%; height: 100%'
+				}
+			});
+	}
+
+	async onClose() {
+		// Nothing to clean up.
 	}
 }
