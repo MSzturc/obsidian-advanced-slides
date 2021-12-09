34719ab1f9b4625faf96d82cf4c723a87d39bc8c
MSzturc <MSzturc@googlemail.com>
2021-12-09 14:07:37 +0100

    download reveal on first launch

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { URL } from 'url';
 import { ICON_DATA } from './constants';
 import { RevealPreviewView, REVEAL_PREVIEW_VIEW } from './revealPreviewView';
 import { RevealServer } from './revealServer';
+import { version } from '../package.json';
+import path from 'path';
+import { existsSync, outputFileSync } from 'fs-extra';
+import request from 'request';
+import JSZip from 'jszip';
 
 export default class AdvancedSlidesPlugin extends Plugin {
 
@@ -16,13 +21,53 @@ export default class AdvancedSlidesPlugin extends Plugin {
 		const fileSystemAdapter: FileSystemAdapter = this.app.vault.adapter as FileSystemAdapter;
 		this.vaultDirectory = fileSystemAdapter.getBasePath();
 
+		const pluginDirectory = path.join(this.vaultDirectory.toString(), '/.obsidian/plugins/obsidian-advanced-slides/');
+		const distDirectory = path.join(pluginDirectory, '/dist/');
+
+		if (!existsSync(distDirectory)) {
+
+			//Download binary
+			const downloadUrl = `https://github.com/MSzturc/obsidian-advanced-slides/releases/download/${version}/obsidian-advanced-slides.zip`;
+
+			let bufs: any = [];
+			let buf: Uint8Array;
+			request
+				.get(downloadUrl)
+				.on('end', () => {
+					buf = Buffer.concat(bufs);
+					var zip = new JSZip();
+					zip.loadAsync(buf).then((contents) => {
+
+						Object.keys(contents.files).forEach(function (filename) {
+							if (!contents.files[filename].dir) {
+								zip.file(filename).async('nodebuffer').then(function (content) {
+									var dest = path.join(pluginDirectory, filename);
+									outputFileSync(dest, content);
+								});
+							}
+
+						});
+					}).catch((error) => {
+						console.log(error);
+					});
+
+				})
+				.on('error', (error) => {
+					console.log(error);
+				})
+				.on('data', (d) => {
+					bufs.push(d);
+				})
+
+		}
+
 		this.revealServer = new RevealServer(this.app, this.vaultDirectory);
 		this.revealServer.start();
 
 		this.registerView(
 			REVEAL_PREVIEW_VIEW,
 			(leaf) => {
-				this.previewView = new RevealPreviewView(leaf,this.revealServer.getUrl());
+				this.previewView = new RevealPreviewView(leaf, this.revealServer.getUrl());
 				return this.previewView;
 			}
 		);
@@ -47,7 +92,7 @@ export default class AdvancedSlidesPlugin extends Plugin {
 			&& this.app.workspace.getLeavesOfType(REVEAL_PREVIEW_VIEW).length > 0) {
 			return;
 		}
-		
+
 		this.target = targetDocument;
 		this.activateView();
 
