2334640c446e814fe45d86e88d977c74475ee086
MSzturc <MSzturc@googlemail.com>
2021-12-16 19:28:20 +0100

    bugfix: invalid range exception in internal note links (#8)

--- src/internalLinkProcessor.ts
+++ src/internalLinkProcessor.ts
@@ -1,28 +1,11 @@
 
 export class InternalLinkProcessor {
 
+	private regex = /\[\[(?:.*\|)?([^\]]*)\]\]/gm;
+
 	process(markdown: string) {
-		return this.transformInternalLink(markdown);
+		return markdown.replaceAll(this.regex,`$1`);
 	}
-
-	private transformInternalLink(line: string): string {
-
-		if (line.includes('[[')) {
-			var startIdx = line.indexOf('[[');
-			var endIdx = line.indexOf(']]', startIdx + 3);
-			var linkContent = line.substring(startIdx + 2, endIdx).trim();
-
-			if (linkContent.includes('|')) {
-				linkContent = linkContent.split('|')[1];
-			}
-			var result = line.substring(0, startIdx) + linkContent + line.substring(endIdx + 2);
-
-			return this.transformInternalLink(result);
-		}
-
-		return line;
-	}
-
 }
 
 
