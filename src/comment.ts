99e9eb4fc6749ad273978acf0abf75350ce88f64
MSzturc <MSzturc@googlemail.com>
2021-12-17 13:16:54 +0100

    commentParser is now able to parse attributes

--- src/comment.ts
+++ src/comment.ts
@@ -2,7 +2,8 @@ export type Comment = {
 
 	type: string,
 	style: string[],
-	clazz: string[]
+	clazz: string[],
+	attributes: Map<string, string>,
 }
 
 export class CommentParser {
@@ -11,13 +12,14 @@ export class CommentParser {
 	private styleRegex = /(?:style="([^"]+)")|(?:style='([^']+)')/;
 	private classRegex = /(?:class="([^"]+)")|(?:class='([^']+)')/;
 	private parseRegex = /<!--\s*\.(element|slide):\s*(.*)-->/;
+	private parsePropertiesRegex = /([^=]*)\s*=\s*"([^"]*)"\s*|([^=]*)\s*=\s*'([^']*)'\s*/g;
 
 	commentToString(comment: Comment): string {
 		return `<!-- .${comment.type}: ${this.buildAttributes(comment)} -->`;
 	}
 
 	buildAttributes(comment: Comment): string {
-		return `style="${comment.style.join('; ')}" class="${comment.clazz.join(' ')}"`;
+		return `style="${comment.style.join('; ')}" class="${comment.clazz.join(' ')}" ${this.attributeMapToString(comment.attributes)}`;
 	}
 
 	parseLine(line: string): Comment {
@@ -29,20 +31,29 @@ export class CommentParser {
 	parseComment(comment: string): Comment {
 		try {
 			var [, type, properties] = this.parseRegex.exec(comment);
-			var [style, clazz] = this.parseProperties(properties);
-			return this.buildComment(type, style, clazz);
+			var [style, clazz, attributes] = this.parseProperties(properties);
+			return this.buildComment(type, style, clazz, attributes);
 		} catch (ex) {
 			console.log("ERROR: Cannot parse comment: " + comment);
 			return null;
 		}
 	}
 
-	buildComment(type: string, style: string[] = [], clazz: string[] = []): Comment {
+	buildComment(type: string, style: string[] = [], clazz: string[] = [], attributes: Map<string, string> = new Map<string, string>()): Comment {
 		return {
 			type: type,
 			style: style,
 			clazz: clazz,
+			attributes: attributes,
 		}
+	}
+
+	private attributeMapToString(attributes: Map<string, string>): string {
+		var result = [];
+		for (let [key, value] of attributes) {
+			result.push(`${key}="${value}"`);
+		}
+		return result.join(" ");
 	}
 
 	private lineHasComment(line: string): boolean {
@@ -53,8 +64,8 @@ export class CommentParser {
 		return this.readCommentRegex.exec(line)?.[0] ?? '';
 	}
 
-	private parseProperties(properties: string): [string[], string[]] {
-		var result: [string[], string[]] = [[], []];
+	private parseProperties(properties: string): [string[], string[], Map<string, string>] {
+		var result: [string[], string[], Map<string, string>] = [[], [], new Map<string, string>()];
 
 		if (properties.includes('style=')) {
 			result[0] = this.parseStyle(properties);
@@ -63,6 +74,8 @@ export class CommentParser {
 		if (properties.includes('class=')) {
 			result[1] = this.parseClass(properties);
 		}
+
+		result[2] = this.parseAttributes(properties);
 
 		return result;
 	}
@@ -79,6 +92,36 @@ export class CommentParser {
 		return result.split(" ").map((attr) => {
 			return attr.trim();
 		});
+	}
+
+	private parseAttributes(properties: string): Map<string, string> {
+		let m;
+		let attributes = new Map<string, string>();
+
+		while ((m = this.parsePropertiesRegex.exec(properties)) !== null) {
+			if (m.index === this.parsePropertiesRegex.lastIndex) {
+				this.parsePropertiesRegex.lastIndex++;
+			}
+
+			var key: string;
+			var value: string;
+
+			m.forEach((match, groupIndex) => {
+				if (groupIndex == 1 || groupIndex == 3) {
+					key = match;
+				}
+				if (groupIndex == 2 || groupIndex == 4) {
+					value = match;
+					attributes.set(key, value);
+				}
+			});
+
+		}
+
+		attributes.delete(undefined);
+		attributes.delete('style');
+		attributes.delete('class');
+		return attributes;
 	}
 
 }
