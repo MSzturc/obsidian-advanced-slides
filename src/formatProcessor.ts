1b3643d266dfaf8f04961cae35f8ef8411133800
MSzturc <MSzturc@googlemail.com>
2021-12-18 17:07:38 +0100

    bugfix: underscore separator (___) is broken (#13)

--- src/formatProcessor.ts
+++ src/formatProcessor.ts
@@ -1,12 +1,15 @@
 
 export class FormatProcessor {
 
-	private regex = /==([^=]*)==/gm;
+	private boldRegex = /(?:^|[^_])__([^_]+)__(?:[^_]|$)/gm;
+	private markRegex = /==([^=]*)==/gm;
 	private commentRegex = /%%([^%]*)%%/gm;
 
 	process(markdown: string){
-		return markdown.replaceAll('__','**')
-		.replaceAll(this.regex,'<mark>$1</mark>')
+		return markdown.replaceAll(this.boldRegex,(sub, args) =>{
+			return `**${args.trim()}**`;
+		})
+		.replaceAll(this.markRegex,'<mark>$1</mark>')
 		.replaceAll(this.commentRegex,'');
 	}
 
