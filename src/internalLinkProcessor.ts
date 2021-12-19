6cb7375576e097f22a67616475f9bfd6f74e7a00
MSzturc <MSzturc@googlemail.com>
2021-12-19 23:13:17 +0100

    bugfix: internal links not rendered properly (#14)

--- src/internalLinkProcessor.ts
+++ src/internalLinkProcessor.ts
@@ -8,7 +8,7 @@ export class InternalLinkProcessor {
 		this.utils = utils;
 	}
 
-	private regex = /(?:^|[^!])\[\[(?:.*\|)?([^\]]*)\]\]/gm;
+	private regex = /(?<=[^!]|^)\[\[(?:.*\|)?([^\]]*)\]\]/gm;
 
 	process(markdown: string, options: any) {
 		if(options.enableLinks){
