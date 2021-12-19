052e1d6f01e1cd3b7b13be8689f795fefea63de6
MSzturc <MSzturc@googlemail.com>
2021-12-19 17:09:29 +0100

    added unit-test for fragments

--- test/extendedSyntax.unit.test.ts
+++ test/extendedSyntax.unit.test.ts
@@ -102,3 +102,31 @@ no color
 	return expect(sut.process(markdown, options)).toMatchSnapshot();
 });
 
+test('Extended Markdown Syntax >  Fragments', () => {
+
+	const input =
+`Fade in <!-- .element: class="fragment" -->
+
+Fade out <!-- .element: class="fragment fade-out" -->
+
+Highlight red <!-- .element: class="fragment highlight-red" -->
+
+Fade in, then out <!-- .element: class="fragment fade-in-then-out" -->
+
+Slide up while fading in <!-- .element: class="fragment fade-up" -->
+
+---
+
+- Permanent item
+- Appear Fourth <!-- .element: class="fragment" data-fragment-index="4" -->
+- Appear Third <!-- .element: class="fragment" data-fragment-index="3" -->
+- Appear Second <!-- .element: class="fragment" data-fragment-index="2" -->
+- Appear First <!-- .element: class="fragment" data-fragment-index="1" -->
+`;
+
+	const { options, markdown } = prepare(input);
+	var sut = new ObsidianMarkdownPreprocessor(utilsInstance);
+
+	return expect(sut.process(markdown, options)).toMatchSnapshot();
+});
+
