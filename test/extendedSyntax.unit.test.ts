2a1281ace647c1ad3943bf9761510ee0c87d36af
MSzturc <MSzturc@googlemail.com>
2021-12-19 17:07:35 +0100

    added unit-test for block comments

--- test/extendedSyntax.unit.test.ts
+++ test/extendedSyntax.unit.test.ts
@@ -63,5 +63,42 @@ Paragraph has coral background color, too!
 	return expect(sut.process(markdown, options)).toMatchSnapshot();
 });
 
+test('Extended Markdown Syntax >  Block Comments', () => {
 
+	const input =
+`::: block
+
+#### Header
+_and_
+Paragraph content
+*in same block*
+
+:::
+
+---
+
+no color
+
+::: block <!-- .element: style="background-color: red;" -->
+
+everything inside this block has red background color
+
+::: block <!-- .element: style="background-color: blue;" -->
+
+blue
+
+:::
+
+red
+
+:::
+
+no color
+`;
+
+	const { options, markdown } = prepare(input);
+	var sut = new ObsidianMarkdownPreprocessor(utilsInstance);
+
+	return expect(sut.process(markdown, options)).toMatchSnapshot();
+});
 
