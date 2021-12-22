64eaf5d3b55b84369b85135a1d152271f7d6422c
MSzturc <MSzturc@googlemail.com>
2021-12-22 13:59:57 +0100

    code cleanup

--- src/markdown.js
+++ src/markdown.js
@@ -1,7 +1,9 @@
 export const md = (() => {
     // Hack required since https://github.com/hakimel/reveal.js/commit/d780352b7f78e16635ce9fabf2dbb53639610f18
+    // eslint-disable-next-line no-undef
     global.Reveal = {
         registerPlugin: () => {}
     };
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     return require('reveal.js/plugin/markdown/markdown')();
 })();
