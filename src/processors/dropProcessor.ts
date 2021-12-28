5cf5cfe59e8cd2f33adeb2e2b81bcf7ba3242d25
MSzturc <MSzturc@googlemail.com>
2021-12-28 13:25:58 +0100

    bugfix: only one slide annotation is allowed

--- src/processors/dropProcessor.ts
+++ src/processors/dropProcessor.ts
@@ -1,7 +1,11 @@
+import { CommentParser } from "src/comment";
 import { Options } from "src/options";
 
 export class DropProcessor {
 	private dropRegex = /drop="[^"]+"/;
+	private slideCommentRegex = /<!--\s*(?:\.)?slide.*-->/;
+
+	private parser = new CommentParser();
 
 	process(markdown: string, options: Options) {
 		let output = markdown;
@@ -27,7 +31,16 @@ export class DropProcessor {
 	}
 
 	transformSlide(slide: string) {
-		return slide + '\n<!-- .slide: class="drop" -->\n'
+
+		if(this.slideCommentRegex.test(slide)){
+			const [match] = this.slideCommentRegex.exec(slide);
+			const comment = this.parser.parseLine(match);
+			comment.addClass("drop");
+			return slide.replace(this.slideCommentRegex,this.parser.commentToString(comment));
+		} else {
+			return slide + '\n<!-- .slide: class="drop" -->\n'
+		}
+
 	}
 
 }
