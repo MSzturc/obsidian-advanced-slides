fce4e8b205d4e11c377f9ff40257e366abfb3966
MSzturc <MSzturc@googlemail.com>
2022-01-02 01:41:15 +0100

    copy images to export dir

--- src/revealExporter.ts
+++ src/revealExporter.ts
@@ -8,10 +8,12 @@ export class RevealExporter {
 
 	private pluginDirectory: string;
 	private exportDirectory: string;
+	private vaultDirectory: string;
 
 	constructor(utils: ObsidianUtils) {
 		this.pluginDirectory = utils.getPluginDirectory();
 		this.exportDirectory = utils.getExportDirectory();
+		this.vaultDirectory = utils.getVaultDirectory();
 	}
 
 	public async export(filePath: string, html: string, imgList: string[]) {
@@ -26,6 +28,13 @@ export class RevealExporter {
 		await copy(path.join(this.pluginDirectory, 'css'), path.join(folderDir, 'css'));
 		await copy(path.join(this.pluginDirectory, 'dist'), path.join(folderDir, 'dist'));
 		await copy(path.join(this.pluginDirectory, 'plugin'), path.join(folderDir, 'plugin'));
+
+		for (const img of imgList) {
+			if(img.startsWith("http")){
+				continue;
+			}
+			await copy(path.join(this.vaultDirectory, img), path.join(folderDir, img));
+		}
 
 		window.open('file://' + folderDir);
 	}
