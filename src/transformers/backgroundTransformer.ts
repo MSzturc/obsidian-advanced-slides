f2edc38d11cb005c4613512bf529624e18fcf19c
MSzturc <MSzturc@googlemail.com>
2022-01-04 16:32:33 +0100

    feature: bg property does now support images

--- src/transformers/backgroundTransformer.ts
+++ src/transformers/backgroundTransformer.ts
@@ -1,30 +1,46 @@
 import Color from "color";
 import { AttributeTransformer, Properties } from ".";
 
-export class BackgroundTransformer implements  AttributeTransformer {
-  
+export class BackgroundTransformer implements AttributeTransformer {
+
 	transform(element: Properties) {
 		const bg = element.getAttribute('bg');
 
-		if(bg != undefined){
-			const color = Color(bg);
+		if (bg != undefined) {
+			const color = this.readColor(bg);
 
-			if(color.isLight()){
-				element.addClass('has-light-background');
-				element.deleteClass('has-dark-background');
+			if (color) {
+				if (color.isLight()) {
+					element.addClass('has-light-background');
+					element.deleteClass('has-dark-background');
+				} else {
+					element.addClass('has-dark-background');
+					element.deleteClass('has-light-background');
+				}
+
+				element.deleteAttribute('bg');
+				const target = element.getAttribute('onTarget');
+				if (target && target == "slide") {
+					element.addAttribute('data-background-color', bg);
+				} else {
+					element.addStyle('background-color', bg);
+				}
 			} else {
-				element.addClass('has-dark-background');
-				element.deleteClass('has-light-background');
+				element.deleteAttribute('bg');
+				const target = element.getAttribute('onTarget');
+				if (target && target == "slide") {
+					element.addAttribute('data-background-image', bg);
+				} 
 			}
 
-			element.deleteAttribute('bg');
-			const target = element.getAttribute('onTarget');
-			if(target && target == "slide"){
-				element.addAttribute('data-background-color',bg);
+		}
+	}
 
-			} else {
-				element.addStyle('background-color', bg);
-			}
+	readColor(bg: string) {
+		try {
+			return Color(bg);
+		} catch (_) {
+			return null;
 		}
 	}
 }
