1bea9aad189bfe40caf979ac357c50ed004023aa
MSzturc <MSzturc@googlemail.com>
2022-03-25 10:56:48 +0100

    bugfix: Footnotes not rendered properly in tables (#55)

--- src/processors/footNoteProcessor.ts
+++ src/processors/footNoteProcessor.ts
@@ -4,39 +4,39 @@ export class FootnoteProcessor {
 
 	private regex = /\[\^([^\]]*)]/gmi;
 
-	process(markdown: string, options: Options){
+	process(markdown: string, options: Options) {
 
 		let output = markdown;
 
 		markdown
-        .split(new RegExp(options.separator, 'gmi'))
-        .map((slidegroup, index) => {
-            return slidegroup
-                .split(new RegExp(options.verticalSeparator, 'gmi'))
-                .map((slide, index) => {
-                    if (this.regex.test(slide)) {
-						const newSlide = this.transformFootNotes(slide);
-						output = output.replace(slide, newSlide);
-                        return newSlide;
-                    }
-                    return slide;
-                })
-                .join(options.verticalSeparator);
-        })
-        .join(options.separator);
-    return output;
+			.split(new RegExp(options.separator, 'gmi'))
+			.map((slidegroup, index) => {
+				return slidegroup
+					.split(new RegExp(options.verticalSeparator, 'gmi'))
+					.map((slide, index) => {
+						if (this.regex.test(slide)) {
+							const newSlide = this.transformFootNotes(slide);
+							output = output.replace(slide, newSlide);
+							return newSlide;
+						}
+						return slide;
+					})
+					.join(options.verticalSeparator);
+			})
+			.join(options.separator);
+		return output;
 	}
 
 	transformFootNotes(markdown: string) {
 
 		let input = markdown;
 		let noteIdx = 1;
-	
+
 		const footNotes = new Map();
-	
-		let reResult : RegExpExecArray;
+
+		let reResult: RegExpExecArray;
 		while (reResult = this.regex.exec(input)) {
-	
+
 			input = input
 				.split('\n')
 				.map((line, index) => {
@@ -48,36 +48,36 @@ export class FootnoteProcessor {
 							return "";
 						} else {
 							const split = line.split(reResult[0]);
-	
+
 							let result = split[0].trim();
 							result += '<sup id="fnref:' + reResult[1] + '" role="doc-noteref">' + noteIdx + '</sup>';
-							result += '\n' + split[1].trim();
-	
+							result += split[1].trim();
+
 							noteIdx = noteIdx + 1;
-	
+
 							return result;
-	
+
 						}
 					}
 					return line;
 				})
 				.join('\n');
-	
-	
+
+
 		}
-	
+
 		let footNotesBlock = '';
 		footNotesBlock += '<div class="footnotes" role="doc-endnotes">\n';
 		footNotesBlock += '<ol>\n';
-	
+
 		footNotes.forEach((value, key) => {
 			footNotesBlock += '<li id="fn:' + key + '" role="doc-endnote"><p>' + value + '&nbsp;</p></li>';
 		});
-	
-	
+
+
 		footNotesBlock += '</ol>\n';
 		footNotesBlock += '</div>\n';
-	
+
 		return input + '\n' + footNotesBlock;
 	}
 
