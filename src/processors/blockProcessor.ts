4a6d3725df5d078223a5ea64c27ef2bc8c413729
MSzturc <MSzturc@googlemail.com>
2022-05-09 00:05:44 +0200

    remove debug log

--- src/processors/blockProcessor.ts
+++ src/processors/blockProcessor.ts
@@ -6,7 +6,6 @@ export class BlockProcessor {
 	transformBlock(markdown: string) {
 		markdown = markdown.replaceAll(/:::\sblock\s*/g, '<div class="block">\n\n');
 		markdown = markdown.replaceAll(':::', '</div>\n\n');
-		console.log(markdown);
 		return markdown;
 	}
 }
