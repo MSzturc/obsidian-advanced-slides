3fd83cb692b7d7925a46b8142c42b5a7cb0e9f7c
MSzturc <MSzturc@googlemail.com>
2022-06-14 12:26:53 +0200

    bugfix: fixed proper path encoding (#99) (#98) (#96)

--- src/main.ts
+++ src/main.ts
@@ -303,10 +303,17 @@ export default class AdvancedSlidesPlugin extends Plugin {
 		await this.activateView();
 
 		const url = this.revealServer.getUrl();
-		url.pathname = this.target.path;
+		url.pathname = this.fixedEncodeURIComponent(this.target.path);
 
 		this.openUrl(url);
 	}
+
+	private fixedEncodeURIComponent(str: string) {
+		return str.replace(/[!'()*]/g, function (c) {
+			return '%' + c.charCodeAt(0).toString(16);
+		});
+	}
+
 
 	private async openUrl(url: URL) {
 		const instance = this.getViewInstance();
