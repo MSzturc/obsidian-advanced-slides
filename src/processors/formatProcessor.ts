219d03d98c54c3c873832e01515b32464c7862d2
MSzturc <MSzturc@googlemail.com>
2022-08-26 23:07:05 +0200

    bugfix: format bold should not remove whitespace after string

--- src/processors/formatProcessor.ts
+++ src/processors/formatProcessor.ts
@@ -1,5 +1,5 @@
 export class FormatProcessor {
-	private boldRegex = /(?<=[^_]|^)__([^_]+)__(?:[^_]|$)/gm;
+	private boldRegex = /(?<=[^_]|^)__([^_]+)__(?!_)/gm;
 	private markRegex = /==([^=]*)==/gm;
 	private commentRegex = /%%([^%]*)%%/gm;
 
