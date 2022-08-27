7819ac330287a9d2b374a9ab4beafd4d1cacfed0
MSzturc <MSzturc@googlemail.com>
2022-08-27 10:53:54 +0200

    test: added test for chart support

--- test/extendedSyntax.unit.test.ts
+++ test/extendedSyntax.unit.test.ts
@@ -286,6 +286,28 @@ test('Extended Markdown Syntax >  Excalidraw support', () => {
 	return expect(sut.process(markdown, options)).toMatchSnapshot();
 });
 
+test('Extended Markdown Syntax >  Chart support', () => {
+
+	const input = `
+	
+	\`\`\`chart
+    type: bar
+    labels: [Monday,Tuesday,Wednesday,Thursday,Friday, Saturday, Sunday, "next Week", "next Month"]
+    series:
+      - title: Title 1
+        data: [1,2,3,4,5,6,7,8,9]
+      - title: Title 2
+        data: [5,4,3,2,1,0,-1,-2,-3]
+\`\`\`
+
+	`;
+
+	const { options, markdown } = prepare(input);
+	const sut = new ObsidianMarkdownPreprocessor(utilsInstance);
+
+	return expect(sut.process(markdown, options)).toMatchSnapshot();
+});
+
 test('Advanced Slides Feature >  Show Debug Grid', () => {
 	const input = `## My Slide
 
